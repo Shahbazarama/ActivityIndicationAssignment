c15cb5b02591c1998905bb11b7a68e5d842e08d7
Shahbazarama <raffi.p.shahbazian@gmail.com>
2016-04-27 14:27:33 -0700

    finished

--- src/ai.js
+++ src/ai.js
@@ -1,9 +1,18 @@
 var AI = {
-  show: function(){
-    var mu = "<div class='fadeBox'> <div class='spinner'> </div> </div>";
+  show: function(style){
+		var styleTest = style;
+    if(styleTest === "style1"){
+		var mu = "<div class='fadeBox1'> <div class='spinner1'> </div> </div>";
+		} else if(styleTest === "style2"){
+		var mu = "<div class='fadeBox2'> <div class='spinner2'> </div> </div>";
+	  } else{
+		var mu = "<div class='fadeBox'> <div class='spinner'> </div> </div>";
+		}
     document.body.innerHTML += mu;
   },
   hide: function(){
     $(".fadeBox").remove();
+		$(".fadeBox1").remove();
+		$(".fadeBox2").remove();
   }
 }
